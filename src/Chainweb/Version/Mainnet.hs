0ae65303926566c6d04c02a4467cb3962fdcb6f0
Edmund Noble <edmundnoble@gmail.com>
2025-12-11 12:50:12 -0500

    Set chainweb310 fork height on mainnet

--- src/Chainweb/Version/Mainnet.hs
+++ src/Chainweb/Version/Mainnet.hs
@@ -156,8 +156,8 @@ mainnet = ChainwebVersion
         Chainweb229Pact -> AllChains (ForkAtBlockHeight $ BlockHeight 5_785_923) -- 2025-05-01 00:00:00+00:00
         Chainweb230Pact -> AllChains (ForkAtBlockHeight $ BlockHeight 6_027_616) -- 2025-07-24 00:00:00+00:00
         Chainweb231Pact -> AllChains (ForkAtBlockHeight $ BlockHeight 6_269_344) -- 2025-10-16 00:00:00+00:00
-        Chainweb31 -> AllChains ForkNever
         MigratePlatformShare -> AllChains (ForkAtBlockHeight $ BlockHeight 6_335_858) -- 2025-11-07 04:00:00+00:00
+        Chainweb31 -> AllChains (ForkAtBlockHeight $ BlockHeight 6_510_742) -- 2026-01-08 00:00:00+00:00
 
     , _versionGraphs =
         (to20ChainsMainnet, twentyChainGraph) `Above`
